0a90edb6209d451aebeaebae4354eedd1dc9030a
Gerred Gruenspan <ggruenspan@outlook.com>
2023-09-02 18:16:20 -0400

    Fixed Styling

--- src/components/errorPage.jsx
+++ src/components/errorPage.jsx
@@ -3,11 +3,11 @@ import '../assets/css/errorStyle.css';
 function errorPage() {
     return (
         <>
-            <div className='errorDiv'>
+            <div className="errorDiv">
                 <h1> Error404: Cannot Reach Page</h1>
                 <p>This page either cannot be reached or does not exist</p>
                 <p>If you like to go back to the home page click the link below</p>
-                <a href="https://yourtriptoparadise.netlify.app" className='errorLink'>https://yourtriptoparadise.netlify.app</a>
+                <p><a href="https://yourtriptoparadise.netlify.app">https://yourtriptoparadise.netlify.app</a></p>
                 <p>We are truly sorry for this inconvenience</p>
             </div>
         </>
